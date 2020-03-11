d24fe19d927d916812d74d22483bd4781d1f4353
Luke Uritescu <Luritescu@gmail.com>
2020-03-10 23:15:58 -0500

    Finished chapter 6 of the Unity AIbook

---
+++ AIChap6/Assets/Scripts/VehicleAvoidance.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleAvoidance : MonoBehaviour
+{
+    public float speed = 20.0f;
+    public float mass = 5.0f;
+    public float force = 50.0f;
+    public float minimumDistToAvoid = 20.0f;
+
+    //Actual speed othe vehicle
+    private float curSpeed;
+    private Vector3 targetPoint;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        mass = 5.0f;
+        targetPoint = Vector3.zero;
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label("CLick anywhere to move the vehicle to the clicked point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vehicle move by mouse click
+        RaycastHit hit;
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if(Input.GetMouseButtonDown(0) && Physics.Raycast(ray, out hit, 100.0f))
+        {
+            targetPoint = hit.point;
+        }
+
+        //Directional vector to the target position
+        Vector3 dir = (targetPoint - transform.position);
+        dir.Normalize();
+
+        //Apply obstacle avoidance
+        AvoidObstacles(ref dir);
+
+        //Don't move the vehiclewhen the target point is reached
+        if (Vector3.Distance(targetPoint, transform.position) < 3.0f)
+            return;
+        //Assign the speed with delta time
+        curSpeed = speed * Time.deltaTime;
+
+        //Rortate the vehicle to its target directional vector 
+        var rot = Quaternion.LookRotation(dir);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rot, 5.0f * Time.deltaTime);
+
+        //Move the vehicle towards
+        transform.position += transform.forward * curSpeed;
+    }
+
+    public void AvoidObstacles(ref Vector3 dir)
+    {
+        RaycastHit hit;
+        //Only detect layer 8 (Obstacles)
+        int layerMask = 1 << 8;
+
+        //Check that the vehicle hit with obstacles within it's minimum distance to avoid
+        if(Physics.Raycast(transform.position, transform.forward, out hit, minimumDistToAvoid, layerMask)){
+            //Get the normal of the hit point to calculate the new direction
+            Vector3 hitNormal = hit.normal;
+            hitNormal.y = 0.0f; //DOn't want to move in Y-space
+
+            //Get the new directional vector by addign force to vehicle's current forward vector
+            dir = transform.forward + hitNormal * force;
+        }
+    }
+}
---
+++ AIChap6/Assets/Scripts/VehicleFollowing.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleFollowing : MonoBehaviour
+{
+    public Path path;
+    public float speed = 20.0f;
+    public float mass = 5.0f;
+    public bool isLooping = true;
+
+    //Actual speed of the vehicle
+    private float curSpeed;
+
+    private int curPathIndex;
+    private float pathLength;
+    private Vector3 targetPoint;
+
+    Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pathLength = path.Length;
+        curPathIndex = 0;
+
+        //get the current velocity of the vehicle
+        velocity = transform.forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Unifty the speed
+        curSpeed = speed * Time.deltaTime;
+        targetPoint = path.GetPoint(curPathIndex);
+
+        //if reach the radius within the path then move to next poitn in the path
+        if(Vector3.Distance(transform.position, targetPoint) < path.Radius)
+        {
+            //Don't move the vehicle if path is finished
+            if (curPathIndex < pathLength - 1)
+                curPathIndex++;
+            else if (isLooping)
+                curPathIndex = 0;
+            else
+                return;
+        }
+
+        //Move the vehicle unti lthe end point is reached in the path
+        if (curPathIndex >= pathLength)
+            return;
+
+        //Calculate the next velocity toward the path
+        if (curPathIndex >= pathLength - 1 && !isLooping)
+            velocity += Steer(targetPoint, true);
+        else
+            velocity += Steer(targetPoint);
+
+        transform.position += velocity; // Move the vehicle according to the velocity
+        transform.rotation = Quaternion.LookRotation(velocity); //Rotate the vehicle towards the desired velocity
+        
+    }
+
+    //Steering algorithm to steer the vector towards the target
+    public Vector3 Steer(Vector3 target, bool bFinalPoint = false)
+    {
+        //Calculate the directional vector from the current position towards the target point
+        Vector3 desiredVelocity = (target - transform.position);
+        float dist = desiredVelocity.magnitude;
+
+        //Normalize the desired Velocity
+        desiredVelocity.Normalize();
+
+        //Calculate the velocity accordign to the speed
+        if (bFinalPoint && dist < 10.0f)
+            desiredVelocity *= (curSpeed * (dist / 10.0f));
+        else
+            desiredVelocity *= curSpeed;
+        //Calculate the force vector
+        Vector3 steeringForce = desiredVelocity - velocity;
+        Vector3 acceleration = steeringForce / mass;
+
+        return acceleration;
+    }
+}
